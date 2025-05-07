b0886b55f52563d1d1877c2e8ac5cd64f0a1a016
Osnildo <juniorosnildo777@gmail.com>
2025-05-07 18:07:12 -0300

    Adiciona classe equipamentos e seus metodos além de uma classe auxiliar pra gerar Ids

---
+++ Equipamento.cs
@@ -0,0 +1,27 @@
+﻿namespace GestaoDeEstoque;
+
+public class Equipamento(
+    string nome,
+    double precoDeAquisicao,
+    string numeroDeSerie,
+    DateTime dataDeAquisicao,
+    string fabricante)
+{
+    public int Id { get; } = GeradorDeId.GerarId();
+    public string Nome { get; set; } = nome;
+    public double  PrecoDeAquisicao { get; set; } = precoDeAquisicao;
+    public string NumeroDeSerie { get; set; } = numeroDeSerie;
+    public DateTime DataDeAquisicao { get; set; } = dataDeAquisicao;
+    public string Fabricante { get; set; } = fabricante;
+
+    public override string ToString()
+    {
+        return $"-----------------------------------------\n"+
+               $"[{Id}] - {Nome}\n"+
+               $"Preço de aquisição: {PrecoDeAquisicao}\n"+
+               $"Numero de serie: {NumeroDeSerie}\n"+
+               $"Data de aquisiçõa: {DataDeAquisicao}\n"+
+               $"Fabricante: {Fabricante}\n"+
+               $"-----------------------------------------";
+    }
+}
---
+++ GeradorDeId.cs
@@ -0,0 +1,13 @@
+﻿namespace GestaoDeEstoque;
+
+public static class GeradorDeId
+{
+    private static int id = 1;
+    
+    public static int GerarId()
+    {
+        var newId = id;
+        id++;
+        return newId;
+    }
+}
